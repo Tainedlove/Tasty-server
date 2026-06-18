455912e50e23ac91a8adb3fc54f9145ecda56f6d
Tainedlove <lebedev651@gmail.com>
2026-06-18 15:24:54 +0700

    Revert "Delete tasks directory"
    This reverts commit d39d607a858cfb5229569cf1e5256d21de12ae2d.

---
+++ tasks/2task/logsarhivator.sh
@@ -0,0 +1,1 @@
+tar -czf /home/u-m2cld/test1/logs228/"logs_$(date +%Y%m%d_%H%M%S).tar.gz" /home/u-m2cld/test1/logs228/*.log && mv test1/logs228/*.gz test1/arh228/ && rm test1/logs228/*.log || echo Error logs jopa
